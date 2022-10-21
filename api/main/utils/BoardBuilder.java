ec9b23258d48fe1de9b6416347c0cd8d957000f0
Che-tsun Ho <republicho@foxmail.com>
2022-10-21 17:42:03 +0800

    Update API

--- api/main/utils/BoardBuilder.java
+++ api/main/utils/BoardBuilder.java
@@ -4,36 +4,16 @@ import main.model.Pieces.*;
 
 public class BoardBuilder {
 
-    public static Chess chessFactory(String pieceName,
+    /**
+     * Factory method for returning various types of pieces.
+     * @param pieceName
+     * @param sideName
+     * @param row
+     * @param col
+     * @return
+     */
+    public static Piece PieceFactory(String pieceName,
                                      Side sideName, int row, int col){
-
-        if(pieceName.equals("ELE")){
-            return new Elephant(new Location(row, col), sideName);
-        }
-
-        if(pieceName.equals("LIO")){
-            return new Lion(new Location(row, col), sideName);
-        }
-        if(pieceName.equals("TIG")){
-            return new Tiger(new Location(row, col), sideName);
-        }
-        if(pieceName.equals("LEO")){
-            return new Chess(new Location(row, col), Animal.LEO, sideName);
-        }
-        if(pieceName.equals("WOL")){
-            return new Chess(new Location(row, col), Animal.WOL, sideName);
-        }
-        if(pieceName.equals("DOG")){
-            return new Chess(new Location(row, col), Animal.DOG, sideName);
-        }
-
-        if(pieceName.equals("CAT")){
-            return new Chess(new Location(row, col), Animal.CAT, sideName);
-        }
-
-        if(pieceName.equals("RAT")){
-            return new Rat(new Location(row, col), sideName);
-        }
 
         return null;
 
--- api/main/utils/Location.java
+++ api/main/utils/Location.java
@@ -3,18 +3,16 @@ package main.utils;
 public class Location{
 
     private int r,i,c;
+
     public Location(int row, int column){
-        i=(row-1)*7+column-1;
-        r=row;
-        c=column;
+
     }
 
     public Location(int index){
-        i=index;
-        c=index%7+1;
-        r=index/7+1;
+
     }
 
+    // Getters
     public int getRow(){
         return r;
     }
