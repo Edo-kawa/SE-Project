070e651a8c9df7415648d1bd77e785e90f73a8ac
Edward Ye <Edo_kawa@outlook.com>
2022-11-03 19:04:00 +0800

    Updated Main Program

--- src/main/java/MVCPatternDemo.java
+++ src/main/java/MVCPatternDemo.java
@@ -5,12 +5,14 @@ import view.BoardView;
 public class MVCPatternDemo {
     public static void main(String[] args){
         // model
-        ChessBoard chessBoard = new ChessBoard();
+        ChessBoard chessBoard;
         // view
-        BoardView boardView = new BoardView(chessBoard);
+        BoardView boardView;
         // controller
         BoardController controller;
         do {
+            chessBoard = new ChessBoard();
+            boardView = new BoardView(chessBoard);
             controller = BoardController.getController(boardView);
             if (controller == null) {
                 System.out.println("End!");
